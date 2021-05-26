fc2a9308916d91024691741db4f3af915c72721d
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2021-05-26 11:40:16 -0400

    Add tests for Goal and Count and pass

--- extension/core/index.js
+++ extension/core/index.js
@@ -1,6 +1,32 @@
-function turtle(params) {
-  return "hi";
+function Goal() {
 };
 
+Goal.prototype.set = function(goal) {
+  this.description = goal;
+};
 
-module.exports = { turtle };
+Goal.prototype.show = function() {
+  return this.description;
+};
+
+Goal.prototype.complete = function() {
+  this.description = undefined;
+};
+
+Goal.prototype.interupt = function() {
+  this.description = undefined;
+};
+
+function Count() {
+  this.value = 0;
+};
+
+Count.prototype.increment = function() {
+  this.value += 1;
+};
+
+Count.prototype.reset = function() {
+  this.value = 0;
+};
+
+module.exports = { Goal, Count };
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,33 +1,64 @@
-const { turtle } = require('../extension/core/index');
+const { Goal, Count } = require('../extension/core/index');
 
-test('Given X', () => {
-  expect(turtle()).toStrictEqual('hi');
+test('can set goal', () => {
+  goal = new Goal();
+  goal.set('Eat pie');
+  expect(goal.show()).toEqual('Eat pie');
+});
+
+test('can mark goal complete', () => {
+  goal = new Goal();
+  goal.set('Eat pie');
+  goal.complete()
+  expect(goal.show()).toEqual(undefined);
+});
+
+test('can mark goal interupted', () => {
+  goal = new Goal();
+  goal.set('Eat pie');
+  goal.interupt()
+  expect(goal.show()).toEqual(undefined);
+});
+
+test('Can increment count', () => {
+  count = new Count();
+  count.increment();
+  count.increment();
+  expect(count.value).toEqual(2);
+});
+
+test('Can reset count', () => {
+  count = new Count();
+  count.increment();
+  count.increment();
+  count.reset();
+  expect(count.value).toEqual(0);
 });
 
 /*
 
-Set Goal
-Show Goal
-Mark Goal Complete
-Mark Goal Interupted
+X Set Goal
+X Show Goal
+X Mark Goal Complete
+X Mark Goal Interupted
 
-Visit Site
-  On Allow List, allow
-  On Block List, block
-  On neither, prompt
+// Visit Site
+//   On Allow List, allow
+//   On Block List, block
+//   On neither, prompt
 
-Show Prompt
-  Answer Yes
-  Answer No
+// Show Prompt
+//   Answer Yes
+//   Answer No
 
-Add to Allow List
-Remove from Allow List
-Add to Block List
-Remove from Block List
+// Add to Allow List
+// Remove from Allow List
+// Add to Block List
+// Remove from Block List
 
-Increment Goals count
-Increment Distractions count
-Increment Interuptions count
+X Increment Goals count
+X Increment Distractions count
+X Increment Interuptions count
 
 Show Goals Count (per day)
 Show Distractions Count (per day)
