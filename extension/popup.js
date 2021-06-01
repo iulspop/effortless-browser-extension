eb0cb12cead20cfbc5358050a2a82c54fa3ce856
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2021-06-01 08:28:28 -0400

    Set goal to storage by submitting form

---
+++ extension/popup.js
@@ -0,0 +1,11 @@
+const submitButton = document.getElementById("goal-submit-button"),
+      form = document.getElementById("goal-form");
+
+let setGoalToStorage = (event) => {
+  event.preventDefault();
+  let formData = new FormData(form);
+  let goal = formData.get("goal");
+  chrome.storage.local.set({goal: goal});
+};
+
+submitButton.addEventListener("click", setGoalToStorage, true);
