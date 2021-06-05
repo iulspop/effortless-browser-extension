e09876d136676ce3ba483c5c6de81944787f469d
Iuliu Pop <iuliu.laurentiu.pop@gmail.com>
2021-06-05 17:04:28 -0400

    Revert "Change popup elements to be loaded programatically"
    This reverts commit 4dfbe292d43a8f759c976d2f5ac6d1b36f234f67.

--- extension/popup.js
+++ extension/popup.js
@@ -1,40 +1,15 @@
-function createElement(type, props = {}, children = []) {
-  let element = document.createElement(type);
-  Object.assign(element, props);
-  children.forEach(child => element.appendChild(child));
-  return element;
+const submitButton = document.getElementById("goal-submit-button"),
+      form = document.getElementById("goal-form");
+
+let setGoalToStorage = (event) => {
+  event.preventDefault();
+
+  let formData = new FormData(form);
+  let goal = formData.get("goal");
+
+  chrome.storage.local.set({goal: goal});
+
+  chrome.runtime.sendMessage({goalSet: true});
 };
 
-// Create popup programatically
-function createSetGoal() {
-  let label  = createElement("label", {for: "goal", textContent: "Set a goal:"});
-  let input  = createElement("input", {type: "text", name: "goal", id:"goal"});
-  let button = createElement("button", {id: "goal-submit-button", textContent: "Commit"});
-
-  let form = createElement("form", {id: "goal-form"}, [label, input, button]);
-
-  document.body.appendChild(form);
-}
-
-createSetGoal()
-
-// Set event listener
-function addSetGoalListener() {
-  submitButton = document.getElementById("goal-submit-button"),
-  form = document.getElementById("goal-form");
-
-  function setGoalToStorage(event) {
-    event.preventDefault();
-
-    let formData = new FormData(form);
-    let goal = formData.get("goal");
-
-    chrome.storage.local.set({goal: goal});
-
-    chrome.runtime.sendMessage({goalSet: true});
-  };
-
-  submitButton.addEventListener("click", setGoalToStorage, true);
-}
-
-addSetGoalListener()
+submitButton.addEventListener("click", setGoalToStorage, true);
