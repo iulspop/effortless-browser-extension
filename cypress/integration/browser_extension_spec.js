939f513537bf27e19fd6f2185bfa76b60c44e993
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2021-07-27 11:47:42 -0400

    Replace yarn with npm and add cypress

---
+++ cypress/integration/browser_extension_spec.js
@@ -0,0 +1,6 @@
+describe('My First Test', () => {
+  it('try', () => {
+    // cy.visit('https://www.duckduckgo.com');
+    cy.visit('chrome-extension://kfdeejoomejjkcgbjbdgfpbgehdbenob/goal-statistics/statistics.html');
+  });
+});
