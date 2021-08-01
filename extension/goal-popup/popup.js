06bc6cb6a5f1a61df8bb7a069ac086de5646ac59
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2021-08-01 08:33:02 -0400

    Remove unused Chrome API features from popup

--- extension/goal-popup/popup.js
+++ extension/goal-popup/popup.js
@@ -11,19 +11,6 @@ function getFormData(event, name) {
   return formData.get(name);
 }
 
-function setDataToStorage(data) {
-  chrome.storage.local.set(data);
-}
-
-function getFormDataAndSetGoalToStorage(event) {
-  const data = { goal: getFormData(event, 'goal') };
-  setDataToStorage(data);
-}
-
-function removeGoalFromStorage() {
-  chrome.storage.local.remove('goal');
-}
-
 function hideElementToggle(elementId) {
   document.getElementById(elementId).classList.toggle('u-hide');
 }
@@ -32,69 +19,23 @@ function createHideElementToggleClosure(elementId) {
   return () => hideElementToggle(elementId);
 }
 
-function fetchStorage(keys) {
-  return new Promise((resolve) => {
-    chrome.storage.local.get(keys, (items) => resolve(items));
-  });
-}
-
-async function incrementValueInStorage(key) {
-  const items = await fetchStorage([key]);
-  let value = items[key] == null ? 1 : items[key] + 1;
-  setDataToStorage({ [key]: value });
-}
-
-function createIncrementValueClosure(key) {
-  return () => incrementValueInStorage(key);
-}
-
-function openExtensionPage(url) {
-  const fullURL = chrome.runtime.getURL(url);
-
-  function onGot(tabs) {
-    if (tabs.length > 0) {
-      chrome.tabs.update(tabs[0].id, { active: true });
-    } else {
-      chrome.tabs.create({ url: fullURL });
-    }
-    window.close();
-  }
-
-  chrome.tabs.query({ url: fullURL }, onGot);
-}
-
-function openStats() {
-  openExtensionPage('../goal-statistics/statistics.html');
-}
-
-const countUpCompletedGoals = createIncrementValueClosure('completedGoalsCount');
-const countUpInteruptedGoals = createIncrementValueClosure('interuptedGoalsCount');
-const countUpDistractions = createIncrementValueClosure('distractionsCount');
 const toggleHideForm = createHideElementToggleClosure('form-container');
 const toggleHideButtons = createHideElementToggleClosure('buttons-container');
 
 submitButton.addEventListener('click', toggleHideForm, true);
 submitButton.addEventListener('click', toggleHideButtons, true);
-submitButton.addEventListener('click', getFormDataAndSetGoalToStorage, true);
+submitButton.addEventListener('click', getFormData, true);
 
 completeButton.addEventListener('click', toggleHideForm, true);
 completeButton.addEventListener('click', toggleHideButtons, true);
-completeButton.addEventListener('click', removeGoalFromStorage, true);
-completeButton.addEventListener('click', countUpCompletedGoals, true);
 
 interuptButton.addEventListener('click', toggleHideForm, true);
 interuptButton.addEventListener('click', toggleHideButtons, true);
-interuptButton.addEventListener('click', removeGoalFromStorage, true);
-interuptButton.addEventListener('click', countUpInteruptedGoals, true);
 
-distractionButton.addEventListener('click', countUpDistractions, true);
+let goal = false;
 
-statsButton.addEventListener('click', openStats);
-
-chrome.storage.local.get(['goal'], ({ goal }) => {
-  if (goal) {
-    hideElementToggle('form-container');
-  } else {
-    hideElementToggle('buttons-container');
-  }
-});
+if (goal) {
+  hideElementToggle('form-container');
+} else {
+  hideElementToggle('buttons-container');
+}
