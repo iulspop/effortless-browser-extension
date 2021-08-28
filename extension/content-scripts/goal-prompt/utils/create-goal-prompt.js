357e0cc2833d099b6d7bf79eea9cc10b0aec6626
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2021-08-28 10:07:47 -0400

    Improve goal prompt style
    Step 2. Made the input and button just the right size. Made prompt have no background, this makes the input look like it's floating without a container. The background that hides the entire page is still active though.

--- extension/content-scripts/goal-prompt/utils/create-goal-prompt.js
+++ extension/content-scripts/goal-prompt/utils/create-goal-prompt.js
@@ -5,9 +5,11 @@ export function createGoalPrompt() {
       <div class="prompt" data-cy="goal-prompt-popup">
         <form class="form">
           <label class="form__label" for="goal">What outcome do you seek?</label>
-          <div class form__input-container>
+          <div class="form__input-container">
             <input class="form__input" type="text" name="goal" data-cy="goal-input">
-            <button class="form__button" data-cy="start-button">Go</button>
+            <button class="form__button" data-cy="start-button">
+              <svg class="form__button__icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z"/></svg>
+            </button>
           </div>
         </form>
       </div>
