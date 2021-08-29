271e824b04b46b5a56037220f51d0b3ae731a6be
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2021-08-29 10:46:58 -0400

    Refactor goal display tests to be more user focused using testing library

--- cypress/e2e/2-goal-display.js
+++ cypress/e2e/2-goal-display.js
@@ -1,47 +1,46 @@
 describe('Goal Prompt Display', () => {
   context('A goal is set and the user visits a page', () => {
+    const goalText = 'Learn just enough from Cypress docs so I can write my tests'
+
     beforeEach(() => {
-      cy.setExtensionState({goal: 'Learn just enough from Cypress docs so I can write my tests'})
+      cy.setExtensionState({goal: goalText})
 
       cy.visit('/turtle')
     })
 
-    it('user sees goal they set', () => {
-      cy.get('[data-cy=goal-display]').should('be.visible')
-
-      cy.get('[data-cy=goal-display]').should('contain', 'Learn just enough from Cypress docs so I can write my tests')
-    })
-
-    it('user can scroll the page', () => {
-      //Without the wait, Cypress will check the below assertion and succeed, 
-      //without retrying and failing once the class is added.
-      cy.wait(500)
-
-      cy.get('html').should('not.have.class', 'u-disable-scrolling')
-    })
-
     it('user marks goal complete and goal prompt appears', () => {
-      cy.get('[data-cy=complete-button]').click()
+      cy.findByRole('button', {name: /complete/i}).click()
 
-      cy.get('[data-cy=goal-display]').should('not.exist')
-      cy.get('[data-cy=goal-prompt-popup]').should('be.visible')
+      cy.findByText(goalText).should('not.exist')
+      cy.findByLabelText(/what outcome do you seek?/i).should('be.visible')
     });
 
     it('user marks goal interupted and goal prompt appears', () => {
-      cy.get('[data-cy=interupt-button]').click()
+      cy.findByRole('button', {name: /interrupt/i}).click()
 
-      cy.get('[data-cy=goal-display]').should('not.exist')
-      cy.get('[data-cy=goal-prompt-popup]').should('be.visible')
+      cy.findByText(goalText).should('not.exist')
+      cy.findByLabelText(/what outcome do you seek?/i).should('be.visible')
     });
 
     it('user clicks retract button to rectract the display to the left', () => {
-      cy.get('[data-cy=retract-button]').click()
+      cy.get('#indistractable-extension').findByRole('button', {name: '<<'}).click()
 
-      cy.get('[data-cy=goal-display]').should('have.class', 'display--retracted')
+      cy.findByTestId('goal-display').should('have.class', 'display--retracted')
 
-      cy.get('[data-cy=retract-button]').click()
+      cy.get('#indistractable-extension').findByRole('button', {name: '>>'}).click()
 
-      cy.get('[data-cy=goal-display]').should('not.have.class', 'display--retracted')
+      cy.findByTestId('goal-display').should('not.have.class', 'display--retracted')
     });
+
+    it('user can scroll the page', () => {
+      // Covers a bug where the scrolling is disabled when it shouldn't be
+
+      // When Cypress checks if there's not the class, it succeeds.
+      // However, if it retried, it would fail after the class is added.
+      // The wait gives a window for the class to be added and for the test to fail.
+      // It avoids false positives.
+      cy.wait(500)
+      cy.get('html').should('not.have.class', 'u-disable-scrolling')
+    })
   });
 });
--- extension/content-scripts/goal-display/utils/create-goal-display.js
+++ extension/content-scripts/goal-display/utils/create-goal-display.js
@@ -1,26 +1,26 @@
 export function createGoalDisplay(goal) {
   const display = `
     <div id="indistractable-extension">
-      <div class="display" data-cy="goal-display">
+      <div class="display" data-testid="goal-display">
         <p class="display__description">${goal}</p>
         <div>
-          <button class="display__button" data-cy="complete-button">Complete</button>
-          <button class="display__button" data-cy="interupt-button">Interrupted</button>
+          <button class="display__button" data-testid="complete-button">Complete</button>
+          <button class="display__button" data-testid="interupt-button">Interrupted</button>
         </div>
-        <button class="display__retract-button" data-cy="retract-button">&lt;&lt;</button>
+        <button class="display__retract-button" data-testid="retract-button">&lt;&lt;</button>
       </div>
     </div>
   `
   document.body.insertAdjacentHTML('beforeend', display)
 
-  document.querySelector('[data-cy="complete-button"]')
+  document.querySelector('[data-testid="complete-button"]')
           .addEventListener('click', send({goalStatus: true, status: "completed"}), true);
 
-  document.querySelector('[data-cy="interupt-button"]')
+  document.querySelector('[data-testid="interupt-button"]')
           .addEventListener('click', send({goalStatus: true, status: "interrupted"}), true);
 
   const goalDisplay = document.querySelector('#indistractable-extension .display')
-  const retractButton = document.querySelector('[data-cy="retract-button"]')
+  const retractButton = document.querySelector('[data-testid="retract-button"]')
   retractButton.addEventListener('click', alternateSign(retractButton), true)
   retractButton.addEventListener('click', toggleClass(goalDisplay, "display--retracted"), true)
 }
